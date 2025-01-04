eaf3731bcf3fd5e6a3c0206fcc155051a185dee3
sxng33.th <sangeethxramesh@gmail.com>
2025-01-05 00:54:54 +0530

    shadcn js config added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-center">Only Task</h1>
+    </div>
   )
 }
 
