a91c76442bc34db66ae479b9459fc3caea0a8871
Akshay CJ <akshaycj999@gmail.com>
2025-01-05 20:20:37 +0530

    fix: rls auth issue

--- src/utils/todo.util.js
+++ src/utils/todo.util.js
@@ -1,11 +1,17 @@
 import { supabase } from "/src/integrations/supabase/client.js";
 
 export const createTodo = async (task) => {
+    const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+    if (userError) throw userError;
+
     const { data, error } = await supabase
         .from("todos")
         .insert([
             {
-                task,completed:false
+                task,
+                completed:false,
+                user_id: user.id
             },
         ])
         .select();
